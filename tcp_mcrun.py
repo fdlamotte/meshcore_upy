20624a74eee668b249de6d7f304f3c05759a287a
Florent <florent@frizoncorrea.fr>
2025-04-04 22:24:14 +0200

    oups and I should know that

--- tcp_mcrun.py
+++ tcp_mcrun.py
@@ -3,7 +3,7 @@ from socket_con import SocketCon
 from meshcore import MeshCore
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect("mchome", 5000)
+sock.connect(("mchome", 5000))
 
 sc = SocketCon(sock)
 mc = MeshCore(sc)
